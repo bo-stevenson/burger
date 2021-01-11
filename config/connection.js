2fba4e589aeefa094a2f2979a09cbeb594984255
Bo Stevenson <bostevenson1027@gmail.com>
2021-01-10 19:14:27 -0600

    added MySQL connection

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+
+const mysql = require("mysql");
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "mysql",
+    database: "burgers_db",
+  });
+}
+//makes the connection
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Connected at ${connection.threadId} \n`);
+});
+//exports
+module.exports = connection;
